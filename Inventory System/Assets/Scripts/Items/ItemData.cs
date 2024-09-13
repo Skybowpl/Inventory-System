ad7ee66c3dc6dc8e1a4bcfb3abfc55508d2c94ed
Skybowpl <szymus456@gmail.com>
2024-09-13 17:01:37 +0200

    Creating IItemData interface

--- Inventory System/Assets/Scripts/Items/ItemData.cs
+++ Inventory System/Assets/Scripts/Items/ItemData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Scriptable Objects/Item Data")]
-public class ItemData : ScriptableObject
+public class ItemData : ScriptableObject, IItemData
 {
     [SerializeField] private string itemName;
     [SerializeField] private Sprite icon;
