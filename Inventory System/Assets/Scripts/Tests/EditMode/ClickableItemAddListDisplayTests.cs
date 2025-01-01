c46d2b5f187e14e05574344e096333c527f8d05d
Skybowpl <szymus456@gmail.com>
2025-01-01 16:33:39 +0100

    ClickableItemAddListDisplay Tests

---
+++ Inventory System/Assets/Scripts/Tests/EditMode/ClickableItemAddListDisplayTests.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using InventorySystem.UI;
+using NUnit.Framework;
+using UnityEngine;
+using InventorySystem.Storage;
+using UnityEngine.TestTools;
+using InventorySystem.Items;
+using UnityEngine.UI;
+
+namespace InventorySystem.Tests.EditMode
+{
+    public class ClickableItemAddListDisplayTests
+    {
+        private class TestClickableItemAddListDisplay : ClickableItemAddListDisplay
+        {
+            public void SetFields()
+            {
+                GameObject testInventory = new GameObject();
+                inventory = testInventory.AddComponent<Inventory>();
+                inventoryDisplay = testInventory.AddComponent<InventoryListWithAmountsDisplay>();
+            }
+            public void SimulateSlotConfiguartion(IItemData itemData, GameObject listSlot)
+            {
+                ConfigureSlot(itemData, listSlot);
+            }
+
+        }
+
+        [Test]
+        public void ConfigureSlot_SetIconForButton()
+        {
+            //Arrange
+            GameObject displayGameObject = new GameObject();
+            TestClickableItemAddListDisplay display = displayGameObject.AddComponent<TestClickableItemAddListDisplay>();
+            display.SetFields();
+            GameObject listSlot = new GameObject();
+            listSlot.AddComponent<Button>();
+            listSlot.AddComponent<Image>();
+
+            Sprite sprite = Sprite.Create(new Texture2D(1, 1), new Rect(0, 0, 1, 1), new Vector2(0.5f, 0.5f));
+            sprite.name = "TestSprite";
+
+            ItemDataMock sword = new ItemDataMock("sword", sprite, "1");
+
+            //Act
+            display.SimulateSlotConfiguartion(sword, listSlot);
+
+            //Assert
+            Assert.AreEqual(listSlot.GetComponent<Image>().sprite.name, sprite.name, "Icon was not set correctly" );
+        }
+
+    }
+}
